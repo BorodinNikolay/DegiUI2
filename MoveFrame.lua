ed3776def53befb4bd98039ffc677fc22f967603
nborodin86 <nborodin86@gmail.com>
2022-05-15 14:07:03 +0700

    master

---
+++ MoveFrame.lua
@@ -0,0 +1,6 @@
+PlayerFrame:ClearAllPoints()
+PlayerFrame:SetPoint("RIGHT", UIParent, "CENTER", -375, 300)
+PlayerFrame:SetUserPlaced(true);
+TargetFrame:ClearAllPoints()
+TargetFrame:SetPoint("RIGHT", UIParent, "CENTER", -150, 300)
+TargetFrame:SetUserPlaced(true);
